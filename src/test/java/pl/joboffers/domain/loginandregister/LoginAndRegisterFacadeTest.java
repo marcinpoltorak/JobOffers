11830ca5e059492d2ad5cdc46c9a890ba4295adb
martinosso <103454347+martinosso@users.noreply.github.com>
2023-11-19 16:51:40 +0100

    created facades and packages for modules and hexagonal

---
+++ src/test/java/pl/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -0,0 +1,12 @@
+package pl.joboffers.domain.loginandregister;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class LoginAndRegisterFacadeTest {
+
+    @Test
+    public void f(){
+
+    }
+}
---
+++ src/test/java/pl/joboffers/domain/offer/OfferFacadeTest.java
@@ -0,0 +1,11 @@
+package pl.joboffers.domain.offer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class OfferFacadeTest {
+    @Test
+    public void f(){
+
+    }
+}

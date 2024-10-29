2681fbc14b9a2c6d1b46f21940833d92f1338e62
martinosso <103454347+martinosso@users.noreply.github.com>
2024-10-29 14:56:41 +0100

    implemented rest controller and test for offers

---
+++ src/main/java/pl/joboffers/infrastructure/offer/controller/OffersRestController.java
@@ -0,0 +1,24 @@
+package pl.joboffers.infrastructure.offer.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.joboffers.domain.offer.OfferFacade;
+import pl.joboffers.domain.offer.dto.OfferResponseDto;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/offers")
+@AllArgsConstructor
+public class OffersRestController {
+    private final OfferFacade offerFacade;
+
+    @GetMapping
+    public ResponseEntity<List<OfferResponseDto>> findAllOffers(){
+        List<OfferResponseDto> offers = offerFacade.findAllOffers();
+        return ResponseEntity.ok(offers);
+    }
+}

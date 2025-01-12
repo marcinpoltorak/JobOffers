3f1bd579905316cef15ef5fb451562112152ce35
martinosso <103454347+martinosso@users.noreply.github.com>
2025-01-12 13:15:21 +0100

    added separated database container for offer url duplicate error integration test

--- src/integration/java/pl/joboffers/controller/error/OfferUrlDuplicateErrorIntegrationTest.java
+++ src/integration/java/pl/joboffers/controller/error/OfferUrlDuplicateErrorIntegrationTest.java
@@ -2,13 +2,28 @@ package pl.joboffers.controller.error;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.ResultActions;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 import pl.joboffers.BaseIntegrationTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class OfferUrlDuplicateErrorIntegrationTest extends BaseIntegrationTest {
+
+    @Container
+    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry){
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+        registry.add("offers.http.client.config.uri", () -> WIRE_MOCK_HOST);
+        registry.add("offers.http.client.config.port", () -> wireMockServer.getPort());
+    }
 
     @Test
     public void should_return_409_conflict_when_added_second_offer_with_same_offer_url() throws Exception {
